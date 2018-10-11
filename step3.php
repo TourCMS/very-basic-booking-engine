c81a57c1b4e1f627bb8d1414ee094141336f8657
paulslugocki <paulslugocki@gmail.com>
2018-10-11 18:21:22 +0100

    TOURCMS-1146 Short tags, Use statement

--- step3.php
+++ step3.php
@@ -1,71 +1,71 @@
 <?php
 	/*
 		step3.php
-		
-		
+
+
 		Calls "Check Availability" to see actual live availability
 		If there's a choice then that's displayed to the customer
 		Boxes for customer details displayed
-		
+
 	*/
 	$title = "Availability confirmed, enter passenger details";
 	include_once("inc/top.php");
-	
+
 	// Include the config file
 	include('inc/config.php');
-	
-	
+
+
 	$qs = "";
-	
+
 	if($marketplace_account_id == 0)
 	isset($_GET['booking_key']) ? $booking_key = $_GET['booking_key'] : exit();
-	
+
 	isset($_GET['date']) ? $date = $_GET['date'] : exit();
-		
-	
+
+
 	$qs .= "date=" . $date;
-	
+
 	isset($_GET['hdur']) ? $hdur = $_GET['hdur'] :  $hdur = null;
-	
+
 	isset($hdur) ? $qs .= "&hdur=" . $hdur : null;
-	
+
 	isset($_GET['rates']) ? $rate_string = $_GET['rates'] : exit();
-	
+
 	isset($_GET['tour']) ? $tour = (int)$_GET['tour'] : exit();
-	
+
 	isset($_GET['channel']) ? $channel = (int)$_GET['channel'] : exit();
-	
+
 	$rates = explode(",", $rate_string);
-	
+
 	$total_people = 0;
-	
+
 	foreach ($rates as $rate) {
 		if(isset($_GET[$rate])) {
 			$rate_count = (int)$_GET[$rate];
-			
+
 			if($rate_count > 0) {
 				$qs .= "&" . $rate . "=" . $rate_count;
 				$total_people += $rate_count;
 			}
 		}
 	}
-	
+
 	$result = $tourcms->check_tour_availability($qs, $tour, $channel);
-	
-	
+
+
 	isset($result->available_components->component) ? $num_components = count($result->available_components->component) : $num_components = 0;
-	
+
 ?>
 <h1><?php print $title; ?></h1>
-<?php 
+<?php
 if($num_components>0) : ?>
 <p>We have checked availability based on the rate and date selection, the available components should be displayed below alongside boxes for customer detail entry.</p>
 <form method="post" action="step4.php" />
-<?php 
+<?php
 if($marketplace_account_id == 0) :
 ?>
 <input type="hidden" name="booking_key" value="<?php print $booking_key; ?>" />
-<?php 
+<?php
 endif;
 ?>
 <input type="hidden" name="total_people" value="<?php print $total_people; ?>" />
@@ -79,13 +79,13 @@ endif;
 		foreach ($result->available_components->component as $component) {
 			?>
 			<tr>
-				<td><input type="radio" name="component_key" value="<?php print $component->component_key; ?>"<?php 
+				<td><input type="radio" name="component_key" value="<?php print $component->component_key; ?>"<?php
 					($counter==1) ? print "checked" : null;
 				 ?> /></td>
 				<td><?php ($hdur>0) ? null : print $component->date_code ; ?></td>
 				<td><?php print prettify_date($component->start_date); ?></td>
 				<td><?php print ((string)$component->end_date <> (string)$component->start_date ? " to " . prettify_date($component->end_date) : null ); ?></td>
-				<td><?php 
+				<td><?php
 					// For hotel type products, lets show the room type
 					if ($hdur > 0):
 							foreach ($component->price_breakdown->price_row as $price_row) {
@@ -101,7 +101,7 @@ endif;
 			<?php
 			$counter ++;
 		}
-	
+
 ?>
 </table>
 </fieldset>
@@ -130,12 +130,12 @@ endif;
 ?>
 <input type="submit" name="submit" value="Go" />
 </form>
-<?php 
+<?php
 else:
 	print "Sorry no availability, please go back and try a different date / number of passengers";
 endif; ?>
-	
-<?php 
+
+<?php
 	include_once("inc/debug.php");
 	include_once("inc/bottom.php");
  ?>
--- step4.php
+++ step4.php
@@ -1,57 +1,57 @@
 <?php
 	/*
 		step4.php
-		
-		
+
+
 		Builds up an XML document representing the customers booking
 		Posts that XML to TourCMS creating a temporary booking
 		Temporary booking will be holding off space on any departures / rooms selected
-		
+
 	*/
 	$title = "Temporary booking created, prompt for creation of confirmed booking";
 	include_once("inc/top.php");
-	
+
 	// Include the config file
 	include('inc/config.php');
-	
+
 	$component_key = isset($_POST['component_key']) ?  $_POST['component_key'] : null;
 
 	$channel = isset($_POST['channel']) ?  $_POST['channel'] : null;
 
 	$total_people = (int)$_POST['total_people'];
-	
+
 	$titles = $_POST['title'];
 	$firstnames = $_POST['firstname'];
 	$surnames = $_POST['surname'];
 	$email = $_POST['email'];
-	  
-	// Create a new SimpleXMLElement to hold the booking details 
-	$booking = new SimpleXMLElement('<booking />'); 
-	$booking->addChild('total_customers', $total_people); 
-	
+
+	// Create a new SimpleXMLElement to hold the booking details
+	$booking = new SimpleXMLElement('<booking />');
+	$booking->addChild('total_customers', $total_people);
+
 	if($marketplace_account_id == 0)
-		$booking->addChild('booking_key', $_POST['booking_key']); 
-	
-	$components = $booking->addChild('components'); 
+		$booking->addChild('booking_key', $_POST['booking_key']);
+
+	$components = $booking->addChild('components');
 	$component = $components->addChild('component');
 	$component->addChild('component_key', $component_key);
-	
+
 	$customers = $booking->addChild('customers');
-	  
+
 	// Add on the customers
 	for($i=0; $i<$total_people; $i++) {
 		$customer = $customers->addChild('customer');
-		
+
 		$customer->addChild('title', $titles[$i]);
 		$customer->addChild('firstname', $firstnames[$i]);
 		$customer->addChild('surname', $surnames[$i]);
-		
+
 		if($i==0)
 			$customer->addChild('email', $email);
-	} 
-	
+	}
+
 	$result = $tourcms->start_new_booking($booking, $channel);
-	
+
 ?>
 <h1><?php print $title; ?></h1>
 <p>Are you sure you wish to book this Tour/Hotel?</p>
@@ -67,10 +67,10 @@ if(!empty($result->booking->booking_engine_url)) {
 	?>
 	<p>Or <a href="<?php echo $result->booking->booking_engine_url; ?>" target="_blank">hand off to the hosted booking engine to complete this booking</a><br />(if you were building a hand-off to the hosted booking engine for real you would probably have skipped the customer name collection on the previous page).</p>
 	<?php
-}	
+}
 ?>
-	
-<?php 
+
+<?php
 	include_once("inc/debug.php");
 	include_once("inc/bottom.php");
  ?>
